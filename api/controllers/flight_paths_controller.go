e3e2d6815a54d32135dfa42defc9ac69a03e52df
Brian Woods <me@brian.biz>
2022-03-29 06:36:40 -0700

    Working practice test

--- api/controllers/flight_paths_controller.go
+++ api/controllers/flight_paths_controller.go
@@ -53,13 +53,78 @@ func (server *Server) GetStartAndDestination(c *gin.Context) {
 	})
 }
 
-func StartAndDestination(flightPaths FlightPaths) []string {
-	var beginAndEnd []string
+func StartAndDestination(flightPaths FlightPaths) [2]string {
+	startingAirports := make(map[string][2]string)
+	endingAirports := make(map[string][2]string)
 
-	for _, flightPath := range flightPaths {
-		beginAndEnd = append(beginAndEnd, flightPath[0])
-		beginAndEnd = append(beginAndEnd, flightPath[1])
+	// first loop
+	for _, flight := range flightPaths {
+		// we've seen the starting point before, therefore there is a loop
+		if _, ok := startingAirports[flight[0]]; ok {
+			return [2]string{"", ""}
+		}
+
+		startingAirports[flight[0]] = flight
+
+		if _, ok := endingAirports[flight[1]]; ok {
+			return [2]string{"", ""}
+		}
+
+		endingAirports[flight[1]] = flight
 	}
 
-	return beginAndEnd
+	sortedFlightPaths := make(FlightPaths, 1)
+	sortedFlightPaths[0] = flightPaths[0]
+
+	// first loop going right
+	// fmt.Printf("starting loop, len(flightPaths) is %v\n", len(flightPaths))
+	i := 1
+	for ; i < len(flightPaths); i++ {
+		// fmt.Printf("top to the right loop - sortFlightPaths: %v\n", sortedFlightPaths)
+		// fmt.Printf("looking for airport %v\n", sortedFlightPaths[len(sortedFlightPaths)-1][1])
+		curr := sortedFlightPaths[len(sortedFlightPaths)-1]
+		// if there is a flight that starts at the ending point of this flight
+		if flight, ok := startingAirports[curr[1]]; ok {
+			sortedFlightPaths = append(sortedFlightPaths, flight)
+			// fmt.Println("appending to the right")
+
+			// remove that starting point from the map
+			// fmt.Printf("curr is %v\n", curr)
+			// fmt.Printf("startingAirports is %v\n", startingAirports)
+
+			delete(startingAirports, curr[1])
+			delete(startingAirports, curr[0]) // can't loop back onto self
+			delete(endingAirports, curr[0]) // can't loop back onto self
+		} else {
+			break
+		}
+	}
+
+	// second loop going left
+	for ; i < len(flightPaths); i++ {
+		// fmt.Printf("top to the left loop - sortFlightPaths: %v\n", sortedFlightPaths)
+		// fmt.Printf("looking for airport %v\n", sortedFlightPaths[0][0])
+		curr := sortedFlightPaths[0]
+		if flight, ok := endingAirports[curr[0]]; ok {
+			sortedFlightPaths = append(FlightPaths{flight}, sortedFlightPaths...)
+			// fmt.Println("appending to the left")
+
+			// remove the ending point from the map
+			// fmt.Printf("curr is %v\n", curr)
+			// fmt.Printf("endingAirports is %v\n", endingAirports)
+			delete(endingAirports, curr[0])
+		} else {
+			break
+		}
+	}
+
+	// fmt.Printf("sortedFlightPaths are %v\n", sortedFlightPaths)
+	// fmt.Printf("len sortedFlightPaths is %d and len flightPaths is %d\n", len(sortedFlightPaths), len(flightPaths))
+
+	// somehow we didn't find a cogent flight path
+	if len(flightPaths) != len(sortedFlightPaths) {
+		return [2]string{"", ""}
+	}
+
+	return [2]string{sortedFlightPaths[0][0], sortedFlightPaths[len(sortedFlightPaths)-1][1]}
 }
--- api/controllers/flight_paths_controller_test.go
+++ api/controllers/flight_paths_controller_test.go
@@ -6,10 +6,96 @@ import (
 )
 
 func TestStartAndDestination(t *testing.T) {
-	result := StartAndDestination(FlightPaths{{"A", "B"}, {"B", "C"}, {"C", "D"}})
-	if result[0] != "A" || result[1] != "D" {
-		t.Errorf("Expected %s, got %v", "[A,D]", result)
+	result := [2]string{"", ""}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}})
+	if result[0] != "A" || result[1] != "B" {
+		t.Errorf("Just one element: Expected %s, got %v", "[A,B]", result)
 	} else {
-		fmt.Printf("Got %v\n", result)
+		fmt.Printf("Just one element: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "C"}})
+	if result[0] != "A" || result[1] != "C" {
+		t.Errorf("Two elements in order: Expected %s, got %v", "[A,C]", result)
+	} else {
+		fmt.Printf("Two elements in order: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"B", "C"}, {"A", "B"}})
+	if result[0] != "A" || result[1] != "C" {
+		t.Errorf("Two elements out of order: Expected %s, got %v", "[A,C]", result)
+	} else {
+		fmt.Printf("Two elements out of order: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"E", "F"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Two disjoint elements: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Two disjoint elements: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "C"}, {"C", "D"}})
+	if result[0] != "A" || result[1] != "D" {
+		t.Errorf("Three elements in order: Expected %s, got %v", "[A,D]", result)
+	} else {
+		fmt.Printf("Three elements in order: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"C", "D"}, {"B", "C"}})
+	if result[0] != "A" || result[1] != "D" {
+		t.Errorf("Three elements out of order: Expected %s, got %v", "[A,D]", result)
+	} else {
+		fmt.Printf("Three elements out of order: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "A"}, {"B", "C"}, {"C", "D"}, {"D", "A"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Multiple elements with loop: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Multiple elements with loop: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "C"}, {"C", "D"}, {"D", "A"}})
+	if result[0] != "A" || result[1] != "A" {
+		t.Errorf("Multiple elements, beginning and ending same airport: Expected %s, got %v", "[A,A]", result)
+	} else {
+		fmt.Printf("Multiple elements, beginning and ending same airport: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "C"}, {"C", "D"}, {"D", "A"}, {"J", "K"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Multiple airports, disjoint: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Multiple airports, disjoint: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"A", "B"}, {"B", "Z"}, {"B", "C"}, {"C", "D"}, {"D", "A"}, {"J", "K"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Multiple airports, disjoint: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Multiple airports, disjoint: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"B", "C"}, {"C", "D"}, {"A", "B"}, {"D", "A"}, {"J", "K"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Start is in the middle: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Start is in the middle: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"B", "C"}, {"C", "D"}, {"D", "A"}, {"J", "K"},{"A", "B"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Start is on the end: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Start is on the end: Got %v\n", result)
+	}
+
+	result = StartAndDestination(FlightPaths{{"B", "C"}, {"C", "D"}, {"D", "A"}, {"J", "K"},{"A", "B"}})
+	if result[0] != "" || result[1] != "" {
+		t.Errorf("Start and end swapped: Expected %s, got %v", "['','']", result)
+	} else {
+		fmt.Printf("Start and end swapped: Got %v\n", result)
 	}
 }
